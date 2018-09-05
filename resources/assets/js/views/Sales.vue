4d80d728b3ea0df0cb436a13c14fe6d19612235f
Abeeb Ola <abeebola@hotmail.com>
2018-09-06 00:30:10 +0100

    Disabled tracking of webpack-generated assets

---
+++ resources/assets/js/views/Sales.vue
@@ -0,0 +1,15 @@
+<template>
+    <layout>
+        Sales!!!
+    </layout>
+</template>
+
+<script>
+    import Layout from './../components/Layout'
+    export default {
+        components: {
+            Layout
+        }
+    }
+
+</script>
